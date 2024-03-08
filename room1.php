69e11165a9c09ab417928f00e7ecb74374e94357
zdungpham <dungphamvan973@gmail.com>
2024-03-09 00:25:58 +0700

    last check

---
+++ room1.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ocean Suite Balcony</title>
+    <link rel="stylesheet" href="css/room.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap" rel="stylesheet">
+</head>
+<body>
+    <?php include("html/header.html");?>
+    <section class="section">
+        <div class="center" style="background-image: url(&quot;https://www.monchericruises.com/wp-content/uploads/2020/10/Balcony-1.jpg&quot;);">
+            <div class="title" ><b>Ocean Suite Balcony</b></div>
+            <h2><a href="index.php">HOME </a>> OCEAN SUITE BALCONY</h2>
+        </div>
+        <!--Carousel-->
+        <div class="caro">
+            <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="images/cabin1_2.jpg" class="d-block w-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="images/cabin1_3.jpg" class="d-block w-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="images/cabin1_4.jpg" class="d-block w-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="images/cabin1_1.jpg" class="d-block w-100" alt="...">
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+        
+        <!--content-->
+
+        <div class="contain row">
+            <div class="col-8">
+                
+                <div class="room-details pt-20"> 
+                    <h2 class="room-details_title"><a>Ocean Suite Balcony</a> </h2>
+                        <div class="room-details_text mr-10">Located on the Main deck of the cruise and offer the space of 35 m2, the cabins come with twin beds (1m x 2m) or double bed (2m x 2m2). This is the ideal option with the private balcony which will absolutely make you feel satisfied with a comfortable space, where you can relax and contemplate the marvelous view of the bay. The en-suite bathroom is also sophisticatedly equipped with a standing shower, high-quality amenities and more. Nonsmoking, complimentary water, tea &amp; coffee facilities, central A/C with manual temperature control, Luxury en-suite bathroom, life jacket &amp; other safety equipment, in-room personal safe, in-house telephone, wardrobe, sofa bed, TV, wifi.</div>
+                </div>
+                <div class="room-details">
+                    <h3 class="room-details_title mb-30"><a>Cabin Amenities</a></h3>
+                    <div class="service-box-wrapper row">
+                        <div class="service-box style5 col-4">
+                            <div class="service-box_icon">
+                                <img src="https://www.monchericruises.com/wp-content/themes/monchericruise/images/resturant_1.svg" alt="">
+                            </div>
+                            <div class="service-box_content">
+                                <h3 class="service-box_title">Breakfast</h3>
+                            </div>
+                        </div>
+                        <div class="service-box style5 col-4">
+                            <div class="service-box_icon">
+                                <img src="https://www.monchericruises.com/wp-content/themes/monchericruise/images/wifi_1.svg" alt="">
+                            </div>
+                            <div class="service-box_content">
+                                <h3 class="service-box_title">Always Wi-Fi</h3>
+                            </div>
+                        </div>
+                        <div class="service-box style5 col-4">
+                            <div class="service-box_icon">
+                                <img src="https://www.monchericruises.com/wp-content/themes/monchericruise/images/spa_1.svg" alt="">
+                            </div>
+                            <div class="service-box_content">
+                                <h3 class="service-box_title">Spa Salon</h3>
+                            </div>
+                        </div>
+                        <div class="service-box style5 col-4">
+                            <div class="service-box_icon">
+                                <img src="https://www.monchericruises.com/wp-content/themes/monchericruise/images/swimming_1.svg" alt="">
+                            </div>
+                            <div class="service-box_content">
+                                <h3 class="service-box_title">Swimming Pool</h3>
+                            </div>
+                        </div>
+                        <div class="service-box style5">
+                            <div class="service-box_icon">
+                                <img src="https://www.monchericruises.com/wp-content/themes/monchericruise/images/gym_1.svg" alt="">
+                            </div>
+                            <div class="service-box_content">
+                                <h3 class="service-box_title">GYM Center</h3>
+                            </div>
+                        </div>
+                        <div class="service-box style5">
+                            <div class="service-box_icon">
+                                <img src="https://www.monchericruises.com/wp-content/themes/monchericruise/images/air_1.svg" alt="">
+                            </div>
+                            <div class="service-box_content">
+                                <h3 class="service-box_title">Air Conditioner</h3>
+                            </div>
+                        </div>
+                        <div class="service-box style5">
+                            <div class="service-box_icon">
+                                <img src="https://www.monchericruises.com/wp-content/themes/monchericruise/images/tv.svg" alt="">
+                            </div>
+                            <div class="service-box_content">
+                                <h3 class="service-box_title">Cable TV</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="sidebar">
+                    <div class="tittle"><h3>Book Your Room</h3></div>
+                    <div class="buton">
+                        <div class="">
+                            <button class="text-center"><a href="Reservation.php">BOOK NOW</a></button>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php include("html/footer.html");?>
+</body>
+</html>
